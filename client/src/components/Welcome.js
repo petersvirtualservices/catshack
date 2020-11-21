9ce5860f5b917dc9bf309ae01a2e8b34c693cb47
Charlene Peters <cpeters1402@gmail.com>
2020-11-20 21:02:56 -0600

    message

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -6,7 +6,7 @@ class Welcome extends React.Component {
         <div id='welcome'>
           <h2>Where Fur Pals Can Get Together</h2>
           <p>We joke about how cats love us when we are needed elsewhere, but in truth, we would not trade that type of demanding behavior for anything in the world; in fact, even when that truth has been stereotypically ingrained in our rationale, we still run to local animal shelters and pet stores to buy these lovable creatures. </p>
-          <p>The Cat Shack is designed to help make that transition smoother. After you take our in-depth, under-utilizing scientific quiz, you will be paired with a celebrity cat, who will help you relate to other similar cats available for adoption.</p>
+          <p>The Cat Shack is designed to help make that transition smoother. After you take our in-depth, under-utilizing scientific quiz, you will be paired with a cat personality that will help guide you through your decision.</p>
           <div>
             <label>Type Your Name To Begin:</label>
              <input
