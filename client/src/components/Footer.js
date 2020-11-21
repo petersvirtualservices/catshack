34b237699c1c9aafb28b8a04376374bc64b69c4a
Bryan Courtney <59741945+bcourtney1029@users.noreply.github.com>
2020-11-21 09:42:41 -0600

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Button from '@material-ui/core/Button';
-
-
 
 class Footer extends React.Component {
   render() {
   return (
-    <div id='footer' >
-      <Breadcrumbs aria-label="breadcrumb">
-      <Button color="inherit" onClick={this.props.restartQuiz}>
-        Home
-      </Button>
-      <Button color="inherit" onClick={this.props.loginOrg}>
-        Login
-      </Button>
-      <Button color="inherit" onClick={this.props.registerOrg}>
-        Register
-      </Button>
-    </Breadcrumbs>
-    </div>
+    <>
+      
+    </>
   );
 }
 }
